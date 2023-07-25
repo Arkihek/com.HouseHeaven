762a617e14085dea378b31496aa040915d3a6f1f
ihsan YILMAZ <arkihekQA@gmail.com>
2023-07-25 13:25:27 +0300

    ihsan/v3/25.07

--- src/test/java/pages/UserHomePage_Body.java
+++ src/test/java/pages/UserHomePage_Body.java
@@ -2,15 +2,4 @@ package pages;
 
 public class UserHomePage_Body {
 
-    public static void main(String[] args) {
-        System.out.println("ihsan");
-        System.out.println("deneme");
-        System.out.println("Melih Deneme");
-        System.out.println("mete");
-        System.out.println("Bilal");
-        System.out.println("Yilmaz");
-        System.out.println("Aysegul");
-        System.out.println("okan");   
-        System.out.println("Serkan");
-    }
 }
