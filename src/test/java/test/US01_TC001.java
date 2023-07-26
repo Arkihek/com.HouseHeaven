fb5705fbc989980bc5c28ca3fedf899a474bf48a
Bilal <bfssargin@gmail.com>
2023-07-26 15:11:36 +0300

    Bilal/v2/26.07

--- src/test/java/test/US01_TC001.java
+++ src/test/java/test/US01_TC001.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AdminDashboard;
 import pages.UserHomepage;
@@ -8,10 +9,19 @@ import utilities.Driver;
 
 public class US01_TC001 {
 
+    // Bir ziyaretçi olarak siteyi kullanabilmek icin web sitesine erişebildiğimi doğrulayabilmeliyim.
+
     UserHomepage userHomepage = new UserHomepage();
     @Test
     public void test01(){
+        // Giriş testi yapılmiştir.
         Driver.getDriver().get(ConfigReader.getProperty("url"));
+        String ecpected="https://qa.hauseheaven.com/";
+
+        String actual=Driver.getDriver().getCurrentUrl();
+
+        Assert.assertEquals(actual,ecpected);
+        Driver.closeDriver();
     }
 
 }
