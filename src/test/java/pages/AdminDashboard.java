efef9fd8c18ae0b0bceeb3c6e9bf2effb3369249
Mete Ersu <meteersu1@gmail.com>
2023-07-30 21:42:59 +0300

    mete/v2/30.07

--- src/test/java/pages/AdminDashboard.java
+++ src/test/java/pages/AdminDashboard.java
@@ -1,8 +1,13 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class AdminDashboard {
 
-
+    public AdminDashboard(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 
 
 
